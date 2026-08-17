6d6ef4ba11696d836302d99769eadffaf7935390
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-17 15:59:15 +0530

    q11

---
+++ level1_basics/q11.py
@@ -0,0 +1,12 @@
+# Q11. Swap two numbers using a third variable.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping:")
+print("First number =", num1)
+print("Second number =", num2)
