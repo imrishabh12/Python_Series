a77d06e05945187ac084430a3573f4d20380af73
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-17 15:58:50 +0530

    q10

---
+++ level1_basics/q10.py
@@ -0,0 +1,9 @@
+# Q10. Calculate the average of three numbers.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+average = (num1 + num2 + num3) / 3
+
+print("Average =", average)
