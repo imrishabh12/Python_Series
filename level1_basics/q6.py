2f4b778afaabf0bdfd317b8e28ec95aeb767bb81
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-17 15:56:21 +0530

    q6

---
+++ level1_basics/q6.py
@@ -0,0 +1,10 @@
+# Q6. Calculate the area and perimeter of a rectangle.
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area =", area)
+print("Perimeter =", perimeter)
