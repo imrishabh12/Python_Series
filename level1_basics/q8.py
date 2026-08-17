7b3a9b4e484f1ed4d5bfb86018c31780480084cb
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-17 15:57:52 +0530

    q8

---
+++ level1_basics/q8.py
@@ -0,0 +1,9 @@
+# Q8. Convert kilometers to meters and centimeters.
+
+kilometers = float(input("Enter distance in kilometers: "))
+
+meters = kilometers * 1000
+centimeters = kilometers * 100000
+
+print("Meters =", meters)
+print("Centimeters =", centimeters)
