1dc78408264493f120d22283dcdaa63abcc21be4
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-17 16:01:45 +0530

    q15

---
+++ level1_basics/q15.py
@@ -0,0 +1,9 @@
+# Q15. Take length, width, and height and calculate the volume of a cuboid.
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+height = float(input("Enter height: "))
+
+volume = length * width * height
+
+print("Volume of cuboid =", volume)
