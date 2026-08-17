46e9d8e77c0158efe6265b5cc1781a305170a411
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-17 16:00:08 +0530

    q112

---
+++ level1_basics/q13.py
@@ -0,0 +1,9 @@
+# Q13. Take a number and print its square and cube.
+
+num = float(input("Enter a number: "))
+
+square = num ** 2
+cube = num ** 3
+
+print("Square =", square)
+print("Cube =", cube)
