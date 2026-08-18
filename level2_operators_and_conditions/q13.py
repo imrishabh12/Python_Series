a132ca162f1bda4190c12d5cf4eebb2f022c748e
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-18 12:44:09 +0530

    q13

---
+++ level2_operators_and_conditions/q13.py
@@ -0,0 +1,14 @@
+# Q13. Calculate student grade based on marks.
+
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >= 60:
+    print("Grade D")
+else:
+    print("Grade F")
