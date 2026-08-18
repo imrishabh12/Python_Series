65d30c2e5edcccf459d7b086e9d71ba36c12304c
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-18 12:45:15 +0530

    q15

---
+++ level2_operators_and_conditions/q15.py
@@ -0,0 +1,12 @@
+# Q15. Determine whether a triangle is equilateral, isosceles, or scalene.
+
+side1 = float(input("Enter first side: "))
+side2 = float(input("Enter second side: "))
+side3 = float(input("Enter third side: "))
+
+if side1 == side2 == side3:
+    print("Equilateral triangle")
+elif side1 == side2 or side1 == side3 or side2 == side3:
+    print("Isosceles triangle")
+else:
+    print("Scalene triangle")
