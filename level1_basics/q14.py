deb87114bbab5cd7111d3be24a1b5f06031fd899
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-18 12:37:28 +0530

    1st ques of level 2

--- level1_basics/q14.py
+++ level1_basics/q14.py
@@ -1,6 +1,15 @@
 # Q14. Take a number and display its data type.
 
-num = input("Enter a number: ")
+#num = input("Enter a number: ")
+
+#print("Value =", num)
+#print("Data type =", type(num))
+
+
+
+#Important: input() always returns a str
+#So if you want to actually store it as a number:
+num = int(input("Enter a number: "))
 
 print("Value =", num)
 print("Data type =", type(num))
