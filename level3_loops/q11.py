6bef811db2810085e94fdea49f89c701fd8b646b
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-19 12:33:37 +0530

    q11

---
+++ level3_loops/q11.py
@@ -0,0 +1,14 @@
+# Q11. Find the sum of digits of a number.
+
+num = int(input("Enter a number: "))
+
+num = abs(num)
+
+total = 0
+
+while num > 0:
+    digit = num % 10
+    total += digit
+    num //= 10
+
+print("Sum of digits =", total)
