cb68960097ac968f1cbbceaafbfd0fa34aa14d6f
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-19 12:34:35 +0530

    q13

---
+++ level3_loops/q13.py
@@ -0,0 +1,18 @@
+# Q13. Check whether a number is prime.
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a prime number")
+else:
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Prime number")
+    else:
+        print("Not a prime number")
