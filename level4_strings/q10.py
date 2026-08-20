3e7bcba0771c33d24517efe7b6b60abf9922fddb
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-20 22:45:36 +0530

    q10

---
+++ level4_strings/q10.py
@@ -0,0 +1,15 @@
+# Q10. Find the frequency of each character.
+
+text = input("Enter a string: ")
+
+frequency = {}
+
+for char in text:
+
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+for char, count in frequency.items():
+    print(char, "=", count)
