38fcffab21f9729fcf5ec487e216039eed2d0fc9
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-21 13:08:29 +0530

    q15

---
+++ level5_list_and_2dlists/q15.py
@@ -0,0 +1,14 @@
+# Q15. Merge two lists.
+
+list1 = [10, 20, 30]
+list2 = [40, 50, 60]
+
+merged = []
+
+for num in list1:
+    merged.append(num)
+
+for num in list2:
+    merged.append(num)
+
+print("Merged list =", merged)
