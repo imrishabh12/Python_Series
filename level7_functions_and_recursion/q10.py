1a54fd8e719decd5a1d3fbe552ba6f1c05f3a653
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-23 01:18:35 +0530

    q10

---
+++ level7_functions_and_recursion/q10.py
@@ -0,0 +1,13 @@
+# Q10. Create a function using keyword arguments.
+
+def student(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+
+student(
+    course="B.Tech",
+    name="Rishabh",
+    age=21
+)
