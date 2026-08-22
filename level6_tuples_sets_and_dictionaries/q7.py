9e7e9a859bc16429446735d4093e12727a94a1f5
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-22 17:33:57 +0530

    q7

---
+++ level6_tuples_sets_and_dictionaries/q7.py
@@ -0,0 +1,8 @@
+# Q7. Remove duplicates from a list using a set.
+
+numbers = [1, 2, 2, 3, 4, 4, 5, 5]
+
+unique_numbers = list(set(numbers))
+
+print("Original list:", numbers)
+print("After removing duplicates:", unique_numbers)
