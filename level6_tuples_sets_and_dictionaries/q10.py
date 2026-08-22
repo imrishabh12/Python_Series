53e59946f7429f894fd2d6f44e6e304b36030a7a
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-22 17:42:50 +0530

    q10

---
+++ level6_tuples_sets_and_dictionaries/q10.py
@@ -0,0 +1,10 @@
+# Q10. Iterate through keys and values.
+
+student = {
+    "name": "Rishabh",
+    "age": 21,
+    "course": "B.Tech"
+}
+
+for key, value in student.items():
+    print(key, ":", value)
