545ff3060689e96b85fad8e7f30ac345680015a2
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-23 01:22:19 +0530

    q14

---
+++ level7_functions_and_recursion/q14.py
@@ -0,0 +1,17 @@
+# Q14. Write a recursive function for Fibonacci.
+
+def fibonacci(n):
+
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input("Enter N: "))
+
+for i in range(n):
+    print(fibonacci(i), end=" ")
