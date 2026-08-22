d169cc1a1431d2b93ff7e9cb75ebe5e0ab890f15
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-23 01:15:35 +0530

    q4

---
+++ level7_functions_and_recursion/q4.py
@@ -0,0 +1,14 @@
+# Q4. Create a function to calculate factorial.
+
+def factorial(n):
+    result = 1
+
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
+
+
+num = int(input("Enter a number: "))
+
+print("Factorial =", factorial(num))
