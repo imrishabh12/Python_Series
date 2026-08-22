0e5d2c0eaa2231de05f87c438f3fd5222bbcaf96
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-22 17:42:23 +0530

    q9

---
+++ level6_tuples_sets_and_dictionaries/q9.py
@@ -0,0 +1,17 @@
+# Q9. Add, update, and delete dictionary elements.
+
+student = {
+    "name": "Rishabh",
+    "age": 21
+}
+
+# Add
+student["course"] = "B.Tech"
+
+# Update
+student["age"] = 22
+
+# Delete
+del student["course"]
+
+print(student)
