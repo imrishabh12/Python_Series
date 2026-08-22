82c4f84b609a06c6cc808569ca86449a68781480
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-23 01:14:19 +0530

    q1

---
+++ level6_tuples_sets_and_dictionaries/q3.py
@@ -0,0 +1,9 @@
+# Q3. Find the maximum and minimum value in a tuple.
+
+numbers = (10, 45, 23, 67, 12)
+
+maximum = max(numbers)
+minimum = min(numbers)
+
+print("Maximum:", maximum)
+print("Minimum:", minimum)
