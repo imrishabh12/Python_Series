f11efe488ce5d8aa8cb653e33c2de2448395e354
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-26 14:37:22 +0530

    q14

---
+++ level10_oops_practice_series/q14.py
@@ -0,0 +1,24 @@
+# Q14. Demonstrate polymorphism.
+
+class Dog:
+
+    def sound(self):
+        print("Dog barks")
+
+
+class Cat:
+
+    def sound(self):
+        print("Cat meows")
+
+
+class Cow:
+
+    def sound(self):
+        print("Cow moos")
+
+
+animals = [Dog(), Cat(), Cow()]
+
+for animal in animals:
+    animal.sound()
