35bb4dbe8b6ff9a10170bd017012e0b8a4642947
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-26 14:34:39 +0530

    q10

---
+++ level10_oops_practice_series/q10.py
@@ -0,0 +1,20 @@
+# Q10. Implement encapsulation using private attributes.
+
+class BankAccount:
+
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+
+account = BankAccount(1000)
+
+account.deposit(500)
+
+print("Balance:", account.get_balance())
