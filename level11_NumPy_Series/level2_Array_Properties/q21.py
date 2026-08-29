03710e7f406990705fea4723b4017456ea4295d7
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-29 21:50:54 +0530

    q21

---
+++ level11_NumPy_Series/level2_Array_Properties/q21.py
@@ -0,0 +1,12 @@
+# Q21. Find the sum and average of all elements in a NumPy array.
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+total = np.sum(arr)
+average = np.mean(arr)
+
+print("Array:", arr)
+print("Sum:", total)
+print("Average:", average)
