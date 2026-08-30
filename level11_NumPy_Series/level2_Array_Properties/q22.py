e7a526996f88ce35fe0d6e6ddfef9111283f846d
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-30 14:36:08 +0530

    q22

---
+++ level11_NumPy_Series/level2_Array_Properties/q22.py
@@ -0,0 +1,12 @@
+# Q22. Find the maximum and minimum elements in a NumPy array.
+
+import numpy as np
+
+arr = np.array([25, 10, 45, 5, 30])
+
+maximum = np.max(arr)
+minimum = np.min(arr)
+
+print("Array:", arr)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
