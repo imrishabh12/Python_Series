71372502e5db7efaa20bf90515fce5f226a67ab9
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-08-31 19:29:30 +0530

    q24

---
+++ level11_NumPy_Series/level2_Array_Properties/q24.py
@@ -0,0 +1,16 @@
+# Q24. Find the transpose of a 2D NumPy array.
+
+import numpy as np
+
+arr = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+transpose = arr.T
+
+print("Original array:")
+print(arr)
+
+print("Transpose:")
+print(transpose)
