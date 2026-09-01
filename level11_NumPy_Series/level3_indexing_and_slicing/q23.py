6b095a70a93fa5179923a3a69c44eaac59b1ad14
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-01 12:14:42 +0530

    q23

---
+++ level11_NumPy_Series/level3_indexing_and_slicing/q23.py
@@ -0,0 +1,8 @@
+# Q23. Access the third and fifth elements of an array.
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50, 60])
+
+print("Third element:", arr[2])
+print("Fifth element:", arr[4])
