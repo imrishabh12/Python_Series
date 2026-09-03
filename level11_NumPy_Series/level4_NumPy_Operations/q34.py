bf31e0265f9c8da68ffd2c6f2c846a2194b330a8
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-03 19:40:36 +0530

    q34

---
+++ level11_NumPy_Series/level4_NumPy_Operations/q34.py
@@ -0,0 +1,10 @@
+# Q34 - Divide two NumPy arrays
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50])
+arr2 = np.array([1, 2, 5, 4, 10])
+
+result = arr1 / arr2
+
+print("Result:", result)
