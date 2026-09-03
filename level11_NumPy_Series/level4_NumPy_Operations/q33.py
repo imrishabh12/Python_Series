51697a1ff160f2736927f240ae4fec7014fd4afd
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-03 19:40:04 +0530

    q33

---
+++ level11_NumPy_Series/level4_NumPy_Operations/q33.py
@@ -0,0 +1,10 @@
+# Q33 - Multiply two NumPy arrays
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50])
+arr2 = np.array([1, 2, 3, 4, 5])
+
+result = arr1 * arr2
+
+print("Result:", result)
