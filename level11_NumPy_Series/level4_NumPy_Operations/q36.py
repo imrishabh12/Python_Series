aaddaf2dbc35376b87b8e24f78e27d7312ec9d82
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-03 19:41:35 +0530

    q36

---
+++ level11_NumPy_Series/level4_NumPy_Operations/q36.py
@@ -0,0 +1,11 @@
+# Q36 - Minimum and maximum
+
+import numpy as np
+
+arr = np.array([25, 10, 45, 5, 30])
+
+minimum = np.min(arr)
+maximum = np.max(arr)
+
+print("Minimum:", minimum)
+print("Maximum:", maximum)
