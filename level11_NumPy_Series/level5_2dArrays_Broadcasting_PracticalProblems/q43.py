f6f5cea139706587d89cf36d35acb2f7ee066f77
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-04 17:49:59 +0530

    q43

---
+++ level11_NumPy_Series/level5_2dArrays_Broadcasting_PracticalProblems/q43.py
@@ -0,0 +1,16 @@
+# Q43 - Find the transpose of a matrix.
+
+import numpy as np
+
+arr = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+transpose = arr.T
+
+print("Original matrix:")
+print(arr)
+
+print("Transpose:")
+print(transpose)
