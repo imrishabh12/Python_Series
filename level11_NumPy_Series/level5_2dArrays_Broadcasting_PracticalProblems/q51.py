f33cf6c3b52e35a54d85e56c1c85593926933192
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-05 21:14:53 +0530

    q51

---
+++ level11_NumPy_Series/level5_2dArrays_Broadcasting_PracticalProblems/q51.py
@@ -0,0 +1,10 @@
+# Q51 - Find the cumulative sum of all elements in a NumPy array.
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+cumulative_sum = np.cumsum(arr)
+
+print("Original array:", arr)
+print("Cumulative sum:", cumulative_sum)
