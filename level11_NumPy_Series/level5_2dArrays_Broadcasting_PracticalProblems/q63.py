2c33a0cf48b74c740a93290f6f633d30d263d922
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-17 12:13:17 +0530

    q63

---
+++ level11_NumPy_Series/level5_2dArrays_Broadcasting_PracticalProblems/q63.py
@@ -0,0 +1,10 @@
+# Q63 - Find the standard deviation of a NumPy array.
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+standard_deviation = np.std(arr)
+
+print("Array:", arr)
+print("Standard deviation:", standard_deviation)
