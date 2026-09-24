d8f70dd83a3dee5ccc2f7594a885267f5824c045
imrishabh12 <rishavkumar2015.rk@gmail.com>
2026-09-24 13:22:19 +0530

    q70

---
+++ level11_NumPy_Series/level5_2dArrays_Broadcasting_PracticalProblems/q70.py
@@ -0,0 +1,12 @@
+# Q70 - Multiply two NumPy arrays element by element.
+
+import numpy as np
+
+arr1 = np.array([2, 3, 4, 5])
+arr2 = np.array([10, 20, 30, 40])
+
+result = arr1 * arr2
+
+print("Array 1:", arr1)
+print("Array 2:", arr2)
+print("Multiplication:", result)
